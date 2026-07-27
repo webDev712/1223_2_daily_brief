57443eae0c2212dcdfb1dfdcc6c8de940dc82f7d
tegic <maksimkruglik2@gmail.com>
2026-07-27 11:59:53 +0200

    adding logs to the neon auth

--- auth.ts
+++ auth.ts
@@ -13,14 +13,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     callbacks: {
         async signIn({ user }) {
             if (!user.email) return false;
-            const users = await sql`
-                SELECT id
-                FROM website_user
-                WHERE email = ${user.email}
+             try {
+                const users = await sql`
+                    SELECT id
+                    FROM website_user
+                    WHERE email = ${user.email}
                     AND archived = false
-                LIMIT 1;
-            `;
-            return users.length > 0;
+                    LIMIT 1;
+                `;
+
+                console.log("DB result:", users);
+
+                return users.length > 0;
+                } catch (e) {
+                console.error("DB ERROR:", e);
+                throw e;
+                }
         }
     }
 });
